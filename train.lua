35a64f225d136dff86b444187f8678712bda87d9
Abhishek Das <das.abhshk@gmail.com>
2015-11-11 22:42:46 -0500

    First 'read' the question, then 'see' the image

--- train.lua
+++ train.lua
@@ -131,7 +131,7 @@ end
 params, grad_params = utils.combine_all_parameters(model)
 
 print('Parameters: ' .. params:size(1))
-print('Batches: ' .. loader.nbatches)
+print('Batches: ' .. loader.batch_data.train.nbatches)
 
 -- initialization
 if do_random_init then
@@ -149,25 +149,20 @@ end
 table.insert(init_state, h_init:clone())
 table.insert(init_state, h_init:clone())
 
-q_length = -1
+clones = {}
+clones = utils.clone_many_times(lstm, loader.q_max_length + 1)
 
 local init_state_global = utils.clone_list(init_state)
 
 feval_val = function(max_batches)
 
-    n = loader.val_nbatches
     count = 0
+    n = loader.data_batch.val.nbatches
     if max_batches ~= nil then n = math.min(n, max_batches) end
 
     for i = 1, n do
 
         q_batch, a_batch, i_batch = loader:next_batch('val')
-
-        if q_length == -1 or q_length ~= q_batch:size(2) then
-            q_length = q_batch:size(2)
-            clones = {}
-            clones = utils.clone_many_times(lstm, q_length + 1)
-        end
 
         qf = ltw:forward(q_batch)
 
@@ -177,16 +172,16 @@ feval_val = function(max_batches)
             imf = imf:cuda()
         end
 
-        rnn_state = {[0] = init_state_global}
         loss = 0
-        lst = clones[1]:forward{imf, unpack(rnn_state[0])}
-        rnn_state[1] = {}
-        for i = 1, #init_state do table.insert(rnn_state[1], lst[i]) end
-        for t = 2, q_length + 1 do
-            lst = clones[t]:forward{qf:select(2,t-1), unpack(rnn_state[t-1])}
+        rnn_state = {[0] = init_state_global}
+
+        for t = 1, loader.q_max_length do
+            lst = clones[t]:forward{qf:select(2,t), unpack(rnn_state[t-1])}
             rnn_state[t] = {}
             for i = 1, #init_state do table.insert(rnn_state[t], lst[i]) end
         end
+
+        lst = clones[loader.q_max_length + 1]:forward{imf, unpack(rnn_state[loader.q_max_length])}
 
         prediction = sm:forward(lst[#lst])
 
@@ -212,12 +207,6 @@ feval = function(x)
 
     q_batch, a_batch, i_batch = loader:next_batch()
 
-    if q_length == -1 or q_length ~= q_batch:size(2) then
-        q_length = q_batch:size(2)
-        clones = {}
-        clones = utils.clone_many_times(lstm, q_length + 1)
-    end
-
     qf = ltw:forward(q_batch)
 
     imf = lti:forward(i_batch)
@@ -228,18 +217,19 @@ feval = function(x)
 
     ------------ forward pass ------------
 
-    rnn_state = {[0] = init_state_global}
     loss = 0
-    lst = clones[1]:forward{imf, unpack(rnn_state[0])}
-    rnn_state[1] = {}
-    for i = 1, #init_state do table.insert(rnn_state[1], lst[i]) end
-    for t = 2, loader.counts[loader.count_idx] + 1 do
-        lst = clones[t]:forward{qf:select(2,t-1), unpack(rnn_state[t-1])}
+    rnn_state = {[0] = init_state_global}
+
+    for t = 1, loader.q_max_length do
+        lst = clones[t]:forward{qf:select(2,t), unpack(rnn_state[t-1])}
         rnn_state[t] = {}
         for i = 1, #init_state do table.insert(rnn_state[t], lst[i]) end
     end
 
+    lst = clones[loader.q_max_length + 1]:forward{imf, unpack(rnn_state[loader.q_max_length])}
+
     prediction = sm:forward(lst[#lst])
+
     loss = criterion:forward(prediction, a_batch)
 
     ------------ backward pass ------------
@@ -247,18 +237,23 @@ feval = function(x)
     dloss = criterion:backward(prediction, a_batch)
     doutput_t = sm:backward(lst[#lst], dloss)
 
-    drnn_state = {[q_length + 1] = utils.clone_list(init_state, true)}
-    drnn_state[q_length + 1][2] = doutput_t
+    drnn_state = {[loader.q_max_length + 1] = utils.clone_list(init_state, true)}
+    drnn_state[loader.q_max_length + 1][2] = doutput_t
 
-    for t = q_length + 1, 2, -1 do
-        dlst = clones[t]:backward({qf:select(2, t-1), unpack(rnn_state[t-1])}, drnn_state[t])
+    dlst = clones[loader.q_max_length + 1]:backward({imf, unpack(rnn_state[loader.q_max_length])}, drnn_state[loader.q_max_length + 1])
+
+    drnn_state[loader.q_max_length] = {}
+    table.insert(drnn_state[loader.q_max_length], dlst[2])
+    table.insert(drnn_state[loader.q_max_length], dlst[3])
+
+    lti:backward(i_batch, dlst[1])
+
+    for t = loader.q_max_length, 1, -1 do
+        dlst = clones[t]:backward({qf:select(2, t), unpack(rnn_state[t-1])}, drnn_state[t])
         drnn_state[t-1] = {}
         table.insert(drnn_state[t-1], dlst[2])
         table.insert(drnn_state[t-1], dlst[3])
     end
-
-    dlst = clones[1]:backward({imf, unpack(rnn_state[0])}, drnn_state[1])
-    lti:backward(i_batch, dlst[1])
 
     _, lstm_dparams = lstm:getParameters()
     lstm_dparams:clamp(-5, 5)
@@ -270,21 +265,21 @@ end
 local optim_state = {learningRate = opt.learning_rate, alpha = opt.decay_rate}
 
 losses = {}
-iterations = opt.max_epochs * loader.nbatches
+iterations = opt.max_epochs * loader.batch_data.train.nbatches
 lloss = 0
 for i = 1, iterations do
     _,local_loss = optim.rmsprop(feval, params, optim_state)
     losses[#losses + 1] = local_loss[1]
 
     lloss = lloss + local_loss[1]
-    local epoch = i / loader.nbatches
+    local epoch = i / loader.batch_data.train.nbatches
 
     if i%10 == 0 then
         print('epoch ' .. epoch .. ' loss ' .. lloss / 10)
         lloss = 0
     end
 
-    if i % loader.nbatches == 0 and opt.learning_rate_decay < 1 then
+    if i % loader.batch_data.train.nbatches == 0 and opt.learning_rate_decay < 1 then
         if epoch >= opt.learning_rate_decay_after then
             local decay_factor = opt.learning_rate_decay
             optim_state.learningRate = optim_state.learningRate * decay_factor -- decay it
